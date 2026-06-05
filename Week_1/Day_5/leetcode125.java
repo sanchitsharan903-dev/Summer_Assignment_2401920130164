2ccafa75400147e104f090c5789e91d94936ed9e
Sanchit Sharan <sanchitsharan903@gmail.com>
2026-06-05 21:01:48 +0530

    Create leetcode125.java

---
+++ Week_1/Day_5/leetcode125.java
@@ -0,0 +1,17 @@
+//Week_1/Day_5/leetcode125.java
+class leetcode125{
+    public boolean isPalindrome(String s) {
+        String rev="";
+        String modified = "";
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(Character.isLetterOrDigit(c)){
+                modified=modified+Character.toLowerCase(c);
+            }
+        }
+    for(int i=modified.length()-1;i>=0;i--){
+        rev=rev+modified.charAt(i);
+    }
+    return modified.equals(rev);
+    }
+}
