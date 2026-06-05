9ce552d63fc513910d1270b7c9d428b77be9c55f
Sanchit Sharan <sanchitsharan903@gmail.com>
2026-06-05 21:19:26 +0530

    Create leetcode14.java

---
+++ Week_1/Day_5/leetcode14.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String p=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(p)!=0){
+                p=p.substring(0,p.length()-1);   
+        }
+    }
+    return p;
+}
+}
