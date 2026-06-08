38edf7c96fa9abcf4a712d41787d89d27f8507c0
Sanchit Sharan <sanchitsharan903@gmail.com>
2026-06-08 13:24:23 +0530

    Update Library_Interface.java

--- Week_1/OOPS/Library_Interface.java
+++ Week_1/OOPS/Library_Interface.java
@@ -1,4 +1,3 @@
-//Library Management System
 @interface
 interface User{
   void Account();
@@ -9,30 +8,27 @@ class Child implements User{
   int age;
   @override
   public void Account(){
-  if(age<15){
+  if(age<15)
     System.out.println("Registered Successfully!");
-  }
-  else{
-    System.out.println("Error! Age must be less than 15"):
- }
+  else
+    System.out.println("Error! Age must be less than 15");
 }
 @override
 public void Book(){
-  if(Book_name_Type.equals("Child")){
-    System.out.println("BOOK Issued!");}
+  if(Book_name_Type.equals("Child"))
+    System.out.println("BOOK Issued!");
   else
     System.out.println("Select child book only!");
 }
-}
 class Adult implements User{
-  int age;
+  int age;      
   String Book_name_Type;
   @override
   public void Account(){
-  if(age<15){
-    System.out.println("You are a child!");}
-  else{
-    System.out.println("Registered Successfully");}
+  if(age<15)
+    System.out.println("You are a child!");
+  else
+    System.out.println("Registered Successfully");
   }
 }
 public class Library_Interface{
